efaa710efc27a416c34d881f592e9808244150d8
sebtaras <sebastian.taras12@gmail.com>
2020-11-30 14:20:46 +0100

    formatting

--- testing/main.js
+++ testing/main.js
@@ -1,28 +1,25 @@
-const {Guitar} = require("./guitar");
-const readline = require('readline');
-const {Chord} = require("./chord");
-const {chords} = require("./chords");
-const {displayChord} = require("./displayChord");
+const { Guitar } = require("./guitar");
+const readline = require("readline");
+const { displayChord } = require("./displayChord");
 const { exit } = require("process");
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
-})
+    output: process.stdout,
+});
 
 let guitar = new Guitar();
 
 asyncReadLine();
 
 function asyncReadLine() {
-    let chordName = rl.question("Enter a chord to display: ", (chord) => {
-        if(chord != ""){
+    let chordName = rl.question("Enter a chord to display: ", chord => {
+        if (chord != "") {
             displayChord(chord, guitar, "open");
             asyncReadLine();
-        }
-        else{
+        } else {
             exit();
             //test
-        }       
+        }
     });
 }
