b0e1b2d121acd25323301bf537190964c08aa943
st-laptop <sebastian.taras12@gmail.com>
2020-12-13 04:05:11 +0100

    html/css foundation

---
+++ Guitar Companion/scripts/constants.js
@@ -0,0 +1,31 @@
+const STRING_NUM = 6;
+const FRET_NUM = 12; //24?
+const ALL_NOTES = [
+  "A",
+  "A#",
+  "Ab",
+  "B",
+  "B#",
+  "Bb",
+  "C",
+  "C#",
+  "Cb",
+  "D",
+  "D#",
+  "Db",
+  "E",
+  "E#",
+  "Eb",
+  "F",
+  "F#",
+  "Fb",
+  "G",
+  "G#",
+  "Gb",
+];
+
+module.exports = {
+  STRING_NUM,
+  FRET_NUM,
+  ALL_NOTES,
+};
